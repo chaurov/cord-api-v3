914f53cef8a2688bc455f30adedb136940ac47b7
Michael Marshall <michaelmarshall220@gmail.com>
2020-01-13 17:40:11 -0600

    first simple draft of listing organizations. just returns all orgs

--- src/components/organization/organization.ts
+++ src/components/organization/organization.ts
@@ -1,8 +1,9 @@
 import { Field, Int, ObjectType, ID } from 'type-graphql';
-import { BaseNode } from '../../common/base-node';
+import { BaseNode } from 'src/common/base-node';
 
 @ObjectType()
-export class Organization extends BaseNode {
-  @Field(type => String, { nullable: true })
+export class Organization extends BaseNode{
+
+  @Field(type => String, {nullable: true})
   name: string;
 }
