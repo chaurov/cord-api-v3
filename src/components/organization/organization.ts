86712005871f00d68550407efb60340402a15723
Michael Marshall <michaelmarshall220@gmail.com>
2020-01-15 16:59:02 -0600

    updated db schema

--- src/components/organization/organization.ts
+++ src/components/organization/organization.ts
@@ -2,8 +2,7 @@ import { Field, Int, ObjectType, ID } from 'type-graphql';
 import { BaseNode } from '../../common/base-node';
 
 @ObjectType()
-export class Organization extends BaseNode{
-
-  @Field(type => String, {nullable: true})
+export class Organization extends BaseNode {
+  @Field(type => String, { nullable: true })
   name: string;
 }
